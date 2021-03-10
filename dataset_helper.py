5a4c68ea134badd7fd0decf16bb0ce0ea775543a
Wilder <friendwilder@yahoo.com.mx>
2021-03-09 23:38:01 -0600

    Implemented task 1: Created NearEarthObject and CloseApproach classes to represent data and basic testing for those classes

--- dataset_helper.py
+++ dataset_helper.py
@@ -6,3 +6,9 @@ with open(json_filename, 'r') as f:
     contents = json.load(f)
     searching_for = [asteroid for asteroid in contents['data'] if asteroid[0] == '2002 PB' and  '2000-Jan-01' in asteroid[3] ]
     print(searching_for)
+
+""" March 9th, 2021
+
+Ran 73 tests in 3.363s
+
+FAILED (failures=50) """
